7a02d0d4b33f3cc028b0d367b20d1f111bc9e510
Priya2422 <priyapandey.2415@gmail.com>
2022-07-13 18:19:53 +0530

    Counter with javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+const btn1=document.querySelector(".increase");
+const btn2=document.querySelector(".reset");
+const btn3=document.querySelector(".decrease");
+const num=document.querySelector(".num");
+let count=0;
+btn1.addEventListener("click",function(){
+ count++;
+ num.textContent=count;
+ if(count<0){
+    num.style.color=rgb(200, 32, 32);
+}else if(count>0){
+    num.style.color="rgb(3, 203, 3)";
+}else if(count==0){
+    num.style.color="#fff";
+}
+})
+btn2.addEventListener("click",function(){
+    num.textContent=0;
+    num.style.color="#fff";
+})
+
+btn3.addEventListener("click",function(){
+    count--;
+    num.textContent=count;
+    if(count<0){
+        num.style.color='rgb(200, 32, 32)';
+    }else if(count>0){
+        num.style.color="rgb(3, 203, 3)";
+    }else if(count==0){
+        num.style.color="#fff";
+    }
+})
